8f05f50a70c8481ea7b0e2847271451474a309ca
Nazareno Rodríguez <nazarenoorodriguez@gmail.com>
2023-06-16 23:00:47 -0300

    refactor: Adapt report view to latest changes

--- js/report/view_report.js
+++ js/report/view_report.js
@@ -4,24 +4,9 @@ let _id = urlParams.get('_id');
 fetch(`http://127.0.0.1:5000/report/id/${_id}`)
   .then(response => response.json())
   .then(report => {
-    let row = createReportTemplate(report);
-    let table = $("#reports tbody");
-    table.append(row);
-
-    const customerIds = [
-      report.top_one_customer._id,
-      report.top_two_customer._id,
-      report.top_three_customer._id
-    ];
-
-    fetchCustomers(customerIds)
-      .then(customers => {
-        let customerTable = $("#customer-table tbody");
-        customers.forEach(customer => {
-          let row = createCustomerTemplate(customer);
-          customerTable.append(row);
-        });
-      })
+    displayReport(report);
+    fetchCustomers(report.top_customers.map(customer => customer._id))
+      .then(customers => displayTopCustomers(customers))
       .catch(error => {
         console.log("Error fetching customers:", error);
       });
@@ -30,49 +15,54 @@ fetch(`http://127.0.0.1:5000/report/id/${_id}`)
     console.log("Error fetching report:", error);
   });
 
-function fetchCustomers(customerIds) {
-  let promises = customerIds.map(customerId => {
-    return fetch(`http://127.0.0.1:5000/customer/id/${customerId}`)
-      .then(response => response.json())
-      .then(customer => customer)
-      .catch(error => {
+
+  const fetchCustomers = async (customerIds) => {
+    let promises = customerIds.map(async (customerId) => {
+      try {
+        const response = await fetch(`http://127.0.0.1:5000/customer/id/${customerId}`);
+        const customer = await response.json();
+        return customer;
+      } catch (error) {
         console.log(`Error fetching customer with ID ${customerId}:`, error);
         return null;
-      });
-  });
-
-  return Promise.all(promises).then(customers =>
-    customers.filter(customer => customer !== null)
-  );
-}
-
-function createCustomerTemplate(customer) {
-  let template = $("#customer-row-template").html();
-  return Mustache.render(template, customer);
-}
+      }
+    });
+    let customers = await Promise.all(promises);
+    customers = customers.filter((customer) => customer !== null);
+    return customers;
+  };
 
 
-function displayTopCustomers(report) {
+
+const displayTopCustomers = (customers) => {
   let table = $("#customer-table tbody");
   table.empty();
 
-  let topCustomers = [
-    report.top_one_customer,
-    report.top_two_customer,
-    report.top_three_customer
-  ];
-
-  topCustomers.forEach((customer, index) => {
+  customers.forEach(customer => {
     let row = createCustomerTemplate(customer);
     table.append(row);
   });
 }
 
-function createReportTemplate(report) {
+const createCustomerTemplate = (customer)=> {
+  let template = $("#customer-row-template").html();
+  return Mustache.render(template, { customer });
+}
+
+const displayReport = (report) => {
+  let table = $("#reports tbody");
+  table.empty();
+
+  let row = createReportTemplate(report);
+  table.append(row);
+}
+
+const createReportTemplate = (report) => {
   let template = $("#report-item-template").html();
   return Mustache.render(template, report);
 }
 
-$(document).ready(function () {
+
+$(document).ready( ()=> {
   loadInformation();
 });
